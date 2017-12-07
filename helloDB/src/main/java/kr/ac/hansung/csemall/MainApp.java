af0a7698193395ba23943770a4e3c6c20235a1af
JHeart <medusa3290@naver.com>
2017-12-07 14:28:27 +0900

    1207commit

--- helloDB/src/main/java/kr/ac/hansung/csemall/MainApp.java
+++ helloDB/src/main/java/kr/ac/hansung/csemall/MainApp.java
@@ -1,5 +1,7 @@
 package kr.ac.hansung.csemall;
 
+import java.util.List;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
@@ -8,10 +10,44 @@ public class MainApp {
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("kr/ac/hansung/conf/beans.xml");// empty container
 																												
-
+		
 		OfferDAO offerDAO = (OfferDAO) context.getBean("offerDAO");
 		
 		System.out.println("The record count is" + offerDAO.getRowCount());
+		
+		List<Offer> offerList = offerDAO.getoffers();
+		
+		for(Offer offer: offerList)
+			System.out.println(offer);
+		
+		Offer offer = new Offer();
+		offer.setName("nyKim");
+		offer.setEmail("nyKim@hansung.ac.kr");
+		offer.setText("an instructor of spring");
+		
+		if(offerDAO.insert(offer)) {
+			System.out.println("Object is inserted success");
+		}else
+			System.out.println("Object insert failed");
+		
+		
+		offer = offerDAO.getoffer("nyKim");
+		offer.setName("Namyun Kim");
+		if(offerDAO.update(offer)) {
+			System.out.println("Object is update success");
+		}else
+			System.out.println("Object update failed");
+		
+		
+		offer = offerDAO.getoffer("Namyun Kim");
+		System.out.println(offer);
+		
+		if(offerDAO.delete(offer.getId())) {
+			System.out.println("Object is delete success");
+		}else
+					System.out.println("Object delete failed");
+		
 		context.close();
 	}
+
 }
